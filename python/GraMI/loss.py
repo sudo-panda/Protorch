Fix exploding gradients

python/GraMI/loss.py
@@ -1,35 +1,44 @@
 import torch.nn.functional as F
 import torch
 
-def GraMI_loss(X, X_hat, adj_mat, V, A, edge_logits, X_hat_prime, X_prime, lambda1=1, lambda2=1):
-    loss_edge = 0
+def GraMI_loss(X, X_hat, adj_mat, V, A, edge_logits, X_hat_prime, X_prime, lambda0=0.5, lambda1=0.1, lambda2=(1.0/15)):
+    loss_edge_mse = 0
     for k in adj_mat.keys():
-        loss_edge += F.binary_cross_entropy_with_logits(edge_logits[k], adj_mat[k], reduction='mean')
-    loss_edge /= len(adj_mat.keys())
+        loss_edge_mse += F.binary_cross_entropy_with_logits(edge_logits[k], adj_mat[k], reduction='mean')
+    loss_edge_mse /= len(adj_mat.keys())
 
-    print(loss_edge)
+    loss_edge_kl = 0
     for k in V.keys():
         mean, log_var = V[k]
-        loss_edge += - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+        loss_edge_kl += - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+    loss_edge_kl /= len(V.keys())
 
-    loss_attr = 0
+    loss_edge = loss_edge_mse + 0.002 * loss_edge_kl
+    print(loss_edge, loss_edge_mse, 0.002 * loss_edge_kl)
+
+    loss_attr_mse = 0
     for k in X_hat.keys():
-        loss_attr += F.mse_loss(X_hat_prime[k], X_hat[k], reduction='mean')
+        loss_attr_mse += F.mse_loss(X_hat_prime[k], X_hat[k], reduction='mean')
+        # print(k, "\n", X_hat_prime[k], "\n", X_hat[k], "\n")
 
         if torch.isnan(X_hat_prime[k]).any() or torch.isinf(X_hat_prime[k]).any():
             print("Target contains NaN or Inf")
 
         if torch.isnan(X_hat[k]).any() or torch.isinf(X_hat[k]).any():
             print("Input contains NaN or Inf")
-    loss_attr /= len(X_hat.keys())
+    loss_attr_mse /= len(X_hat.keys())
+
+    loss_attr_kl = 0
 
     mean, log_var = A
-    loss_attr += - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+    loss_attr_kl += - 0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
 
-    print(loss_attr)
+    loss_attr = loss_attr_mse + 0.002 * loss_attr_kl
+    print(loss_attr, loss_attr_mse, 0.002 * loss_attr_kl)
+
     loss_rmse = 0
     for k in X.keys():
-        loss_rmse += F.mse_loss(X_prime[k], X[k], reduction='sum')
+        loss_rmse += F.mse_loss(X_prime[k], X[k], reduction='mean')
     
         if torch.isnan(X_prime[k]).any() or torch.isinf(X_prime[k]).any():
             print("Target contains NaN or Inf")
@@ -38,5 +47,5 @@ def GraMI_loss(X, X_hat, adj_mat, V, A, edge_logits, X_hat_prime, X_prime, lambd
     loss_rmse = torch.sqrt(loss_rmse)
     
     print(loss_rmse)
-    loss = loss_edge + lambda1 * loss_attr + lambda2 * loss_rmse
+    loss = lambda0 * loss_edge + lambda1 * loss_attr + lambda2 * loss_rmse
     return loss

python/GraMI/model.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import ModuleDict, Module, Linear, Tanh, Dropout, Sequential
+from torch.nn import ModuleDict, Module, Linear, Tanh, BatchNorm1d, Sequential
 import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv, HeteroConv, Linear, GATv2Conv, InnerProductDecoder
 from torch_geometric.nn.pool import global_mean_pool
@@ -13,8 +13,8 @@ class MLP(Module):
             input_dim = in_dim if i == 0 else hidden_dim
             output_dim = out_dim if i == num_layers - 1 else hidden_dim
             layers.append(Linear(input_dim, output_dim))
-            if i < num_layers - 1:
-                layers.append(Tanh())
+            layers.append(Tanh())
+            layers.append(BatchNorm1d(output_dim))
         self.mlp = Sequential(*layers)
 
     def forward(self, x):
@@ -42,7 +42,7 @@ class GraMIInitializer(Module):
         super(GraMIInitializer, self).__init__()
 
         self.MLP1 = ModuleDict({
-            node_type: MLP(data.size(1), data.size(1) // 2, out_dim)
+            node_type: MLP(data.size(1), (data.size(1) + 1) // 2, out_dim)
             for node_type, data in data.x_dict.items()
         })
     
@@ -83,7 +83,7 @@ class GraMIDecoder(Module):
         self.hgnn = HGNN(data, hidden_dim // 2, hidden_dim, num_layers=1)
 
         self.MLP1 = ModuleDict({
-            node_type: MLP(hidden_dim, data.size(1) // 2, data.size(1), num_layers=2)
+            node_type: MLP(hidden_dim, (data.size(1) + 1) // 2, data.size(1), num_layers=2)
             for node_type, data in data.x_dict.items()
         })
     

python/train.py
@@ -8,25 +8,24 @@ from GraMI.model import GraMIModel
 file_list = list(Path("/mnt/E/Workspaces/LLNL/HecBench/heterodatas/").glob("*.pt"))
 
 dataset = FunctionGraphDataset(file_list)
-data = dataset[1]
 
+def single_step(data):
+    adj_mat = get_adj_mat_from_edge_index(data.x_dict, data.edge_index_dict)
+    model = GraMIModel(data, 16, 8)
+    model.train()
 
-adj_mat = get_adj_mat_from_edge_index(data.x_dict, data.edge_index_dict)
-model = GraMIModel(data, 16, 8)
-model.train()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+    optimizer.zero_grad()
 
-optimizer.zero_grad()
+    X_hat, V, A, X_hat_prime, edge_logits, X_prime = model(data.x_dict, data.edge_index_dict)
 
-X_hat, V, A, X_hat_prime, edge_logits, X_prime = model(data.x_dict, data.edge_index_dict)
+    assert torch.Tensor([X_hat[k].shape == X_hat_prime[k].shape for k in X_hat_prime.keys()]).all() == True
+    assert torch.Tensor([data.x_dict[k].shape == X_prime[k].shape for k in X_prime.keys()]).all() == True
+    assert torch.Tensor([adj_mat[k].shape == edge_logits[k].shape for k in data.edge_index_dict.keys()]).all() == True
 
-assert torch.Tensor([X_hat[k].shape == X_hat_prime[k].shape for k in X_hat_prime.keys()]).all() == True
-assert torch.Tensor([data.x_dict[k].shape == X_prime[k].shape for k in X_prime.keys()]).all() == True
-assert torch.Tensor([adj_mat[k].shape == edge_logits[k].shape for k in data.edge_index_dict.keys()]).all() == True
+    loss = GraMI_loss(data.x_dict, X_hat, adj_mat, V, A, edge_logits, X_hat_prime, X_prime)
 
-loss = GraMI_loss(data.x_dict, X_hat, adj_mat, V, A, edge_logits, X_hat_prime, X_prime)
-
-print(loss)
-loss.backward()
-optimizer.step()
+    print(loss)
+    loss.backward()
+    optimizer.step()