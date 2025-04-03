8d9c18be07524166bac8512815c65c9120487e13
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-04-03 15:00:58 +0200

    Add Adaptive1d pooling for variable input sizes

--- python/GraMI/model.py
+++ python/GraMI/model.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import ModuleDict, Module, Linear, Tanh, BatchNorm1d, Sequential
+from torch.nn import ModuleDict, Module, Linear, Tanh, BatchNorm1d, Sequential, AdaptiveAvgPool1d
 import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv, HeteroConv, Linear, GATv2Conv, InnerProductDecoder
 from torch_geometric.nn.pool import global_mean_pool
@@ -53,24 +53,27 @@ class GraMIEncoder(Module):
     def __init__(self, hetero_data, in_dim, out_dim):
         super(GraMIEncoder, self).__init__()
         
-        self.hgnn = HGNN(hetero_data, in_dim, out_dim, num_layers=1)
+        self.hgnn_enc = HGNN(hetero_data, in_dim, out_dim, num_layers=1)
 
         self.node_mu = Linear(out_dim, out_dim)
         self.node_logvar = Linear(out_dim, out_dim)
 
-        self.mlp = MLP(in_dim, in_dim, out_dim, num_layers=3)
+        mlp_in_dim = out_dim * 8
+        self.pool = AdaptiveAvgPool1d(mlp_in_dim)
+        self.mlp = MLP(mlp_in_dim, out_dim * 2, out_dim, num_layers=3)
 
         self.attr_mu = Linear(out_dim, out_dim)
         self.attr_logvar = Linear(out_dim, out_dim)
     
     def forward(self, x_dict, edge_index):
         X_eps = {k: x + torch.normal(0, 1, size=x.shape) for k, x in x_dict.items()}
-        h_V = self.hgnn(X_eps, edge_index)        
+        h_V = self.hgnn_enc(X_eps, edge_index)
         V = {node_type: (self.node_mu(h), self.node_logvar(h)) for node_type, h in h_V.items()}
 
         X_hat = torch.cat([x_dict[node_type] for node_type in x_dict.keys()], dim=0)
         X_hat_eps = X_hat + torch.normal(0, 1, size=X_hat.shape)
-        h_A = self.mlp(X_hat_eps)
+        X_hat_eps_norm = self.pool(X_hat_eps.T)
+        h_A = self.mlp(X_hat_eps_norm)
         A = (self.attr_mu(h_A), self.attr_logvar(h_A))
 
         return V, A
@@ -80,7 +83,7 @@ class GraMIDecoder(Module):
     def __init__(self, data: HeteroData, hidden_dim):
         super(GraMIDecoder, self).__init__()
 
-        self.hgnn = HGNN(data, hidden_dim // 2, hidden_dim, num_layers=1)
+        self.hgnn_dec = HGNN(data, hidden_dim // 2, hidden_dim, num_layers=1)
 
         self.MLP1 = ModuleDict({
             node_type: MLP(hidden_dim, (data.size(1) + 1) // 2, data.size(1), num_layers=2)
@@ -96,7 +99,7 @@ class GraMIDecoder(Module):
         for node_type, Z_Vi in Z_V.items():
             Z_prime[node_type] = torch.tanh(torch.matmul(Z_Vi, Z_A.T))
 
-        X_hat_prime = self.hgnn(Z_prime, edge_index)
+        X_hat_prime = self.hgnn_dec(Z_prime, edge_index)
 
         X_prime = {node_type: self.MLP1[node_type](x) for node_type, x in X_hat_prime.items()}
 
