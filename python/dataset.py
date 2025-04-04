e079cf588daa741384a1075d1c21825315593ab6
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-04-04 14:07:38 +0200

    Add to device to dataset

--- python/dataset.py
+++ python/dataset.py
@@ -2,8 +2,10 @@ import torch
 from torch_geometric.data import HeteroData, Dataset
 
 class FunctionGraphDataset(Dataset):
-    def __init__(self, file_list):
+    def __init__(self, file_list, device="cpu"):
+        super(FunctionGraphDataset, self).__init__()
         self.file_list = file_list
+        self.device = device
 
     def __len__(self):
         return len(self.file_list)
@@ -11,6 +13,7 @@ class FunctionGraphDataset(Dataset):
     def __getitem__(self, idx):
         with open(self.file_list[idx], "rb") as f:
             data: HeteroData = torch.load(f, weights_only=False)
+            data.to(device=self.device)
         
         del data['module', 'symbol', 'value']
         del data['module']
