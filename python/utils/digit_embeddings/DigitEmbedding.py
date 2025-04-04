109c2c89ac9c6d54bc4ce9b7bcaa244a429dbe29
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-04-04 14:11:03 +0200

    Add variable embedding size

--- python/utils/digit_embeddings/DigitEmbedding.py
+++ python/utils/digit_embeddings/DigitEmbedding.py
@@ -5,6 +5,8 @@ import pathlib
 
 script_path = pathlib.Path(__file__).parent.resolve()
 
+embed_size = 3
+
 feat_count = 0
 feature_file = open(str(script_path / "FeatureMap" / "feature_map_file_pg_plus_text_all_digits.txt"), 'r')
 feature_lines = feature_file.readlines()
@@ -15,7 +17,7 @@ for feature_line in feature_lines:
         feature_key = feature_key
         feature_map[feature_key] = feat_count
         feat_count = feat_count + 1
-embeds = nn.Embedding(feat_count, 3)
+embeds = nn.Embedding(feat_count, embed_size)
 
 def is_numeric(txt):
     if re.match(r"^[-+]?[0-9]*\.?[0-9]+(e[-+]?[0-9]+)?$", txt):
@@ -61,7 +63,7 @@ def get_digit_emb_of_number(token):
                 node_embed = embeds(lookup_tensor)
                 digit_embedding_vector.append(node_embed)
             else:
-                digit_embedding_vector.append(torch.Tensor([0.0, 0.0, 0.0]))
+                digit_embedding_vector.append(torch.Tensor([[0.0,] * embed_size]))
 
         digit_embedding_vector = torch.cat(digit_embedding_vector)
 
@@ -72,7 +74,7 @@ def get_digit_emb_of_number(token):
                 node_embed_mult = node_embed * 10
                 digit_pos_vector.append(node_embed_mult)
             else:
-                digit_pos_vector.append(torch.Tensor([0.0, 0.0, 0.0]))
+                digit_pos_vector.append(torch.Tensor([[0.0,] * embed_size]))
 
         digit_pos_vector = torch.cat(digit_pos_vector)
 
@@ -80,7 +82,7 @@ def get_digit_emb_of_number(token):
         final_embedding_sum = torch.sum(final_embedding, axis=0)
         reduced_final_embedding = final_embedding_sum / (torch.max(torch.abs(final_embedding_sum)) + 1)
     else:
-        reduced_final_embedding = torch.Tensor([0.0, 0.0, 0.0])
+        reduced_final_embedding = torch.Tensor([0.0,] * embed_size)
 
     return reduced_final_embedding
 
