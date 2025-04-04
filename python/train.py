1f6c28dc58a4c2ed19220d3a4d53b3b8e8496ced
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-04-04 17:47:18 +0200

    Minor changes

--- python/train.py
+++ python/train.py
@@ -12,6 +12,7 @@ from paths import GraMI_path, top_level_path
 
 device = "cuda"
 epochs =  1000
+train_from_scratch = False
 
 
 writer = SummaryWriter()
@@ -21,13 +22,15 @@ file_list = list((top_level_path / "HecBench" / "heterodatas").glob("*.pt"))
 dataset = FunctionGraphDataset(file_list, device=device)
 
 model = GraMIModel(dataset[0][1], 16, 8)
-if (GraMI_path / "latest.pt").exists():
+
+print(model)
+if train_from_scratch and (GraMI_path / "latest.pt").exists():
     model.load_state_dict(torch.load(GraMI_path / "latest.pt"), strict=True)
 model.to(device)
 model.train()
 
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 
 def single_step(data):
     adj_mat = get_adj_mat_from_edge_index(data.x_dict, data.edge_index_dict)
