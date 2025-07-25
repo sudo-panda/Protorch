cf8e4f695a20b0208767f74671e3ebf889fde7b8
Baidyanath Kundu <kundubaidya99@gmail.com>
2025-07-25 09:58:01 -0700

    Redirect logging to stderr

--- python/train_devmap.sh
+++ python/train_devmap.sh
@@ -5,7 +5,7 @@
 #flux: -t 6h
 #flux: --job-name=devmap_train
 #flux: --output=logs/flux-{{id.dec}}-out.log
-#flux: --error=logs/flux-{{id.dec}}-err.log
+#flux: --error=logs/flux-{{id.dec}}-{{id}}.log
 set -eo pipefail
 
 echo "START TIME: $(date)"
@@ -14,23 +14,24 @@ WORK_DIR="/usr/WS2/LExperts/mltraining/Protorch/python"
 VENV="/usr/WS2/LExperts/mltraining/venvs/toss_4_x86_64_ib_cray"
 CONFIG_NAME="$1"
 
+echo "Config File Name: $CONFIG_NAME"
+
 PWD=`pwd`
 if [[ "$PWD" == "$WORK_DIR" ]]; then
-    echo "In the correct working directory, fixing ..."
+    >&2 echo "Not in the correct working directory, fixing ..."
     cd "$WORK_DIR"
 fi
 
 if [[ "$VIRTUAL_ENV" != "$VENV" ]]; then
-    echo "Not in the correct virtual environment, fixing ..."
+    >&2 echo "Not in the correct virtual environment, fixing ..."
     source "$VENV/bin/activate"
 fi
 
 if [[ -z "$CONFIG_NAME" ]]; then
-    echo "No config file name provided"
+    >&2 echo "No config file name provided"
     exit 1
 fi
 
-echo "Config File Name: $CONFIG_NAME"
 
 python train_devmap.py --config "$CONFIG_NAME"
 
